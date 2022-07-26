e21414f361a054bd399787a7d0c62a398c00e6e7
vivek saud <viveksaud3@gmail.com>
2022-07-26 14:06:17 +0545

    components and props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import RecipeList from "./RecipeList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RecipeList recipes={sampleRecipes} />
     </div>
   );
 }
 
+const sampleRecipes = [
+  {
+    id: 1,
+    name:'Plain Chicken',
+    cookTime: '1:45',
+    servings: 3,
+    instructions: '1. Put salt on chicken \n2. Put chicken in oven \n3. Eat chicken'
+  },
+  {
+    id: 2,
+    name:'Plain Pork',
+    cookTime: '0:45',
+    servings: 5,
+    instructions: '1. Put paprika on pork \n2. Put pork in oven \n3. Eat pork'
+  }
+];
+
 export default App;
+
+
+
